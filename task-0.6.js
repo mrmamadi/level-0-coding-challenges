d6a743e15b135d55dbcf3b6ded73bf69a7b158c7
mrmamadi <mrmamadi@outlook.com>
2021-09-14 09:22:21 +0200

    added comment about arguments

--- task-0.6.js
+++ task-0.6.js
@@ -1,6 +1,6 @@
 function maximum() {
     let highestValue = 0;
-    for (let i = 0; i<arguments.length; i++) {
+    for (let i = 0; i<arguments.length; i++) { // arguments refers to all parameters entered into function
         if (arguments[i] > highestValue) {
             highestValue = arguments[i];
         }
