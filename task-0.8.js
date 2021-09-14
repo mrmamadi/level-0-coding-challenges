64d91e2c2e893be7d39c1a7b69bda1c1bd313b94
mrmamadi <mrmamadi@outlook.com>
2021-09-14 09:42:54 +0200

    added solution to task 0.8

---
+++ task-0.8.js
@@ -0,0 +1,27 @@
+function toHoursAndMinutes(number) {
+    const hours = Math.floor(number/60); // floor method rounds down
+    const minutes = number%60;
+
+    let pluralOrSingularMinutes = ``
+    if (minutes==1) {
+        pluralOrSingularMinutes = `${minutes} minute`;
+    } else if (minutes > 1) {
+        pluralOrSingularMinutes = `${minutes} minutes`;
+    }
+
+    let pluralOrSingularHours = ``
+    if (hours==1) {
+        pluralOrSingularHours = `${hours} hour`;
+    } else if (hours > 1) {
+        pluralOrSingularHours = `${hours} hours`
+    } else {
+        return `${pluralOrSingularMinutes}.`
+    }
+    
+    if (pluralOrSingularMinutes=='') {
+        return `${pluralOrSingularHours}.`
+    } else{  
+        return `${pluralOrSingularHours}, ${pluralOrSingularMinutes}.`
+    }
+}
+console.log(toHoursAndMinutes(60))
