c8af471a0fe7472f7098ee0eb3722c5e5631ddf4
mrmamadi <mrmamadi@outlook.com>
2021-09-14 08:36:51 +0200

    added solution to task 0.10

---
+++ task-0.10.js
@@ -0,0 +1,16 @@
+function commonLetters(word1,word2) {
+    word1 = word1.split('');
+    word2 = word2.split('');
+
+    const combinedArray = [];
+    for (let i=0; i<word1.length; i++) {
+        for (let j=0; j<word2.length; j++) {
+            if (word1[i]==word2[j]) {
+                combinedArray.push(word1[i])
+            }
+        }
+    }
+    combinedSet = new Set(combinedArray);
+    console.log(`Common letters: ${[...combinedSet].toString()}`); //spread syntax
+}
+(commonLetters('house','computers'))
